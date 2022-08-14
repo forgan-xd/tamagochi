fab35f479d87671b17c78930037de9155419e51f
Juandivox <jugevox@gmail.com>
2022-08-14 15:29:23 -0500

    TITULO + SALIDA
    Esto tiene el titulo, resolucion y salida del programa

--- Python.py
+++ Python.py
@@ -1,4 +1,12 @@
 import pygame
 pygame.init
-ventana= pygame.display.set_mode((1366,768))
-pygame.display.set_caption("FORGANGOCHI")pygame.display.set_caption("FORGANGOCHI")
+ventana= pygame.display.set_mode((1366,768)) #resolucion
+pygame.display.set_caption("FORGANGOCHI") #titulo
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == quit:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update() # todo esto es para cerrar la pestaña
