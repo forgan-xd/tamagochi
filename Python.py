c49c05cba9195a3a874346e16eff7ecb9aa336a8
Juandivox <jugevox@gmail.com>
2022-08-14 15:50:05 -0500

    TITULO + RESOLUCION + SALIDA

--- Python.py
+++ Python.py
@@ -1,11 +1,12 @@
-import pygame
+import pygame,sys
+from pygame import *
 pygame.init
 ventana= pygame.display.set_mode((1366,768)) #resolucion
 pygame.display.set_caption("FORGANGOCHI") #titulo
 
 while True:
     for evento in pygame.event.get():
-        if evento.type == quit:
+        if evento.type == QUIT:
             pygame.quit()
             sys.exit()
 
