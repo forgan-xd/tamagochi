9739dda7ff19c666ad4029ed294caecafb973029
Gilberter <juanchodve14@gmail.com>
2022-08-14 15:50:59 -0500

    runin to runing imbeciles

--- Python.py
+++ Python.py
@@ -2,7 +2,7 @@ from platform import python_branch
 import pygame
 pygame.init
 
-runnin = True
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
