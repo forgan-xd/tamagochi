7bee9718e581c4774fb98e571466b4d008bdfd0d
Gilberter <juanchodve14@gmail.com>
2022-08-14 15:49:43 -0500

    logo

--- Python.py
+++ Python.py
@@ -1,7 +1,11 @@
+from platform import python_branch
 import pygame
 pygame.init
 ventana= pygame.display.set_mode((1366,768)) #resolucion
 pygame.display.set_caption("FORGANGOCHI") #titulo
+#Logo
+icon = pygame.image.load('logo ramirez.jpg')
+pygame.display.set_icon(icon)
 
 while True:
     for evento in pygame.event.get():
